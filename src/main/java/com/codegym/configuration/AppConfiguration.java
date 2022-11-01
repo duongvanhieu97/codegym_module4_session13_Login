3fe233c665b725245b998096044b67d025adeedb
Duonghieu97st <Duonghieu97st@gmail.com>
2022-11-01 09:57:40 +0700

    update change hawaii

--- src/main/java/com/codegym/configuration/AppConfiguration.java
+++ src/main/java/com/codegym/configuration/AppConfiguration.java
@@ -10,7 +10,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
@@ -62,6 +64,12 @@ public class AppConfiguration implements WebMvcConfigurer, ApplicationContextAwa
         messageSource.setBasenames("message");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        registry.addInterceptor(interceptor);
     }
     @Bean
     public LocaleResolver localeResolver() {
